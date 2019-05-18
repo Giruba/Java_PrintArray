2f4e18c0f6a35c2501ab7ec9a50d0c46f5db0c57
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-18 18:50:21 +0530

    PrintArrayJava

---
+++ PrintArray/src/PrintArray.java
@@ -0,0 +1,11 @@
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5};
+		for(int index = 0; index < array.length; index++){
+			System.out.println(array[index]);
+		}
+		
+	}
+}
